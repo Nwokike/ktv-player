d0fd668fb277c7aaba370f362ba4c1d8a8eb4506
Onyeka Nwokike <nwokikeonyeka@gmail.com>
2026-08-03 13:30:50 +0100

    fix: register ConsentManager service on page.services per official Flet docs

--- src/services/ad_service.py
+++ src/services/ad_service.py
@@ -57,6 +57,11 @@ class AdService:
             return
         try:
             self._consent_manager = fta.ConsentManager()
+            if (
+                hasattr(self.page, "services")
+                and self._consent_manager not in self.page.services
+            ):
+                self.page.services.append(self._consent_manager)
             await self._consent_manager.request_consent_info_update()
             await self._consent_manager.load_and_show_consent_form_if_required()
             self._can_request_ads = await self._consent_manager.can_request_ads()
